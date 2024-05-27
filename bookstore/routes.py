341d49f52f694069c1a37d8bda6401f2c438b532
kashyap <kashyapkurmilla@gmail.com>
2024-05-27 23:15:00 +0530

    User Info (usr details and edit user)

--- bookstore/routes.py
+++ bookstore/routes.py
@@ -103,4 +103,61 @@ def init_routes(app, db):
             # Handle error appropriately, for example, redirect to home page with a flash message
             flash('Error fetching book details')
             return redirect(url_for('home'))
+    
+    @app.route('/home/my_account', endpoint='my_account')
+    def my_account():
+        # Check if the referrer is the home page
+        if request.referrer and url_for('home') in request.referrer:
+            # Fetch user details from the database based on the current session's email
+            if 'email' in session:
+                user = db.users.find_one({'_id': session['email']})
+                if user:
+                    return render_template('my_account.html', user=user)
+                else:
+                    flash('User not found')
+                    return redirect(url_for('login'))
+            else:
+                flash('Please log in to access your account')
+                return redirect(url_for('login'))
+        else:
+            # If not coming from the home page, redirect to the home page
+            flash('Access denied')
+            return redirect(url_for('home'))
+        
+    @app.route('/home/edit_account', methods=['GET', 'POST'], endpoint='edit_account')
+    def edit_account():
+        if 'email' not in session:
+            flash('Please log in to edit your account details')
+            return redirect(url_for('login'))
+        
+        user = db.users.find_one({'_id': session['email']})
+        if not user:
+            flash('User not found')
+            return redirect(url_for('home'))
+
+        if request.method == 'POST':
+            # Update user information based on form input
+            new_data = {
+                'firstName': request.form['firstName'],
+                'lastName': request.form['lastName'],
+                'address': {
+                    'country': request.form['country'],
+                    'street1': request.form['street1'],
+                    'street2': request.form['street2'],
+                    'city': request.form['city'],
+                    'province': request.form['province'],
+                    'zip': request.form['zip']
+                }
+            }
+            
+            password = request.form.get('password')
+            confirmPassword = request.form.get('confirmPassword')
+            if password and password == confirmPassword:
+                new_data['hashedPassword'] = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+            
+            db.users.update_one({'_id': session['email']}, {'$set': new_data})
+            flash('Account details updated successfully')
+            return redirect(url_for('my_account'))
+
+        return render_template('edit_account.html', user=user)
 
